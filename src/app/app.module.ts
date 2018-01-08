a6816ce876bbc8f05166533420a76348147833bf
Zhinous Shokrani <spring1986@gmail.com>
2018-01-08 13:05:28 +1100

    add template selector component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { AbcComponent } from './abc/abc.component';
+import { TemplateSelectorComponent } from './template-selector/template-selector.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AbcComponent
+    TemplateSelectorComponent
   ],
   imports: [
     BrowserModule,
